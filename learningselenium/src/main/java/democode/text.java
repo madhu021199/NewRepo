97b179a23361849d6625cbfa58cd8c85b68d6931
madhu <rmadhumitha21199@gmail.com>
2024-09-30 15:34:30 +0530

    x architect

--- learningselenium/src/main/java/democode/text.java
+++ learningselenium/src/main/java/democode/text.java
@@ -8,6 +8,7 @@ public class text {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("new architect");
 		List<String> list = new ArrayList<String>();
 		list.add("asdf");
 		list.add("kjhcdvzhcj");
